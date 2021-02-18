5fbe3805da8cda755c7c347e85090ecd05d8bf5a
maisugarhoney <amandarozikurnia@gmail.com>
2021-02-19 00:08:28 +0700

    Revert "Upload Tugas-4"
    This reverts commit d63ed642eaf496a2f89d4295129b16980d0929f9.

--- Tugas-Harian-Part-1/Tugas-4/tugas4.js
+++ Tugas-Harian-Part-1/Tugas-4/tugas4.js
@@ -12,9 +12,9 @@ while(i > 0) {
     console.log(i + " - I will become a frontend developer");
     i -= 2;
 }
-console.log();
 
 // soal 2
+console.log();
 console.log("-----SOAL DUA-----");
 for(var j = 1; j <= 20; j++) {
     if(j%2 != 0) {
@@ -27,29 +27,28 @@ for(var j = 1; j <= 20; j++) {
         console.log(j + " - Berkualitas");
     }
 }
-console.log();
 
 // Soal 3
+console.log();
 console.log("-----SOAL TIGA-----");
-var pola = "";
 
 for(var a = 0; a < 7; a++) {
     for(var b = 0; b <= a; b++) {
-        pola += "#";
+        process.stdout.write("#");
     }
-    pola += "\n";
+    console.log();
 }
-console.log(pola);
 
 // soal 4
+console.log();
 console.log("-----SOAL EMPAT-----");
 
 var kalimat = "saya sangat senang belajar javascript";
 var newKalimat = kalimat.split(" ");
 console.log(newKalimat);
-console.log();
 
 // soal 5
+console.log();
 console.log("-----SOAL LIMA-----");
 
 var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
