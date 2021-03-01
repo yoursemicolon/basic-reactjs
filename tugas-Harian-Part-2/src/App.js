265644e90929713a6df06627dfcbc4f2fba9be1c
maisugarhoney <amandarozikurnia@gmail.com>
2021-03-01 22:44:22 +0700

    Update src/App.js

--- tugas-Harian-Part-2/src/App.js
+++ tugas-Harian-Part-2/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TabelBuah from './Tugas-10/TabelBuah';
 import AppTable from './Tugas-9/AppTable';
+import Timer from './Tugas-11/Timer'
 
 function App() {
   return (
     <div>
       <AppTable/>
       <TabelBuah/>
+      <Timer/>
     </div>
   );
 }
